1d7e9ee424768ed0d669fb9fcb33d78bb4d15656
Alex Rozumey <brain-geek@yandex.ua>
2012-08-29 23:39:34 +0300

    update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829202343) do
+ActiveRecord::Schema.define(:version => 20120829203441) do
 
   create_table "brands", :force => true do |t|
     t.string   "title"
