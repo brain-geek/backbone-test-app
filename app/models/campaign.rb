02d16344b59a6dfdf01db32732cad3b7a600e6fa
Alex Rozumey <brain-geek@yandex.ua>
2012-09-08 21:47:19 +0300

    Add real json data to model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,5 @@
 class Campaign < ActiveRecord::Base
-  attr_accessible :brand_ids, :title
+  attr_accessible :brand_ids, :title, :lang
 
   has_and_belongs_to_many :brands
   validates_presence_of :title
--- spec/fabricators/campaign_fabricator.rb
+++ spec/fabricators/campaign_fabricator.rb
@@ -1,3 +1,7 @@
 Fabricator(:campaign) do
   title    "MyString"
+  lang     "{
+  'italy':['english', 'russian'],
+  'ukraine':['ukrainian', 'russian']
+  }"
 end
