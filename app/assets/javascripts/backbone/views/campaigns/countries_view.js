09385e1fe7355b62db6e123a83ede8cfe9284db6
Alex Rozumey <brain-geek@yandex.ua>
2012-09-09 01:14:08 +0300

    Add similar country deletion

--- app/assets/javascripts/backbone/views/campaigns/countries_view.js
+++ app/assets/javascripts/backbone/views/campaigns/countries_view.js
@@ -2,7 +2,8 @@ BackboneRailsApp.Views.Campaigns.CountriesView = Backbone.View.extend({
   template: JST["backbone/templates/campaigns/countries"],
 
   events: {
-    "click span.remove": "remove"
+    "click .language": "remove_language",
+    "click .country": "remove_country"
   },
 
   initialize: function() {
@@ -15,13 +16,26 @@ BackboneRailsApp.Views.Campaigns.CountriesView = Backbone.View.extend({
     return this;
   },
 
-  remove: function(event) {
+  remove_language: function(event) {
     country = $(event.target).data('country')
     language =  $(event.target).data('lang')
 
     languages = this.model.get('lang')
 
     languages[country].splice(languages[country].indexOf(language), 1)
+
+    //Additional parameter is used because setter plugin does not trigger 'field change' event in backbone
+    this.model.set({'lang': languages, 'lang_changed': (new Date().getTime())})
+
+    return this;
+  },
+
+  remove_country: function(event) {
+    country = $(event.target).data('country')
+
+    languages = this.model.get('lang')
+
+    delete languages[country]
 
     //Additional parameter is used because setter plugin does not trigger 'field change' event in backbone
     this.model.set({'lang': languages, 'lang_changed': (new Date().getTime())})
